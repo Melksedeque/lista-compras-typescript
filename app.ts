b3441cc9dc3d8bc7871608660aa30d1dbc015740
Melksedeque <freelancer@melksedeque.com.br>
2025-03-11 17:55:16 -0300

    Função para salvar itens na lista do localStorage

--- app.ts
+++ app.ts
@@ -3,11 +3,19 @@ interface Item {
   nome: string;
 }
 
-const formularioItem = document.getElementById('formularioItem') as HTMLFormElement;
-const listaItens = document.getElementById('listaItens') as HTMLUListElement;
-const inputItem = document.getElementById('item') as HTMLInputElement;
+const formularioItem = document.getElementById(
+  "formularioItem"
+) as HTMLFormElement;
+const listaItens = document.getElementById("listaItens") as HTMLUListElement;
+const inputItem = document.getElementById("item") as HTMLInputElement;
 
+// Carregando itens do localStorage
 const carregarItens = (): Item[] => {
-    const itens = localStorage.getItem('itens');
-    return itens ? JSON.parse(itens) : [];
+  const itens = localStorage.getItem("itens");
+  return itens ? JSON.parse(itens) : [];
+};
+
+// Salvando itens no localStorage
+const salvarItens = (itens: Item[]) => {
+  localStorage.setItem("itens", JSON.stringify(itens));
 };
