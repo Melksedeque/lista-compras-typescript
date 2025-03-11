88fbabccbec327ab7767879dc4d029cac9ef6c1a
Melksedeque <freelancer@melksedeque.com.br>
2025-03-11 17:54:24 -0300

    Criação de app.ts carregando itens do localStorage

---
+++ app.ts
@@ -0,0 +1,13 @@
+interface Item {
+  id: string;
+  nome: string;
+}
+
+const formularioItem = document.getElementById('formularioItem') as HTMLFormElement;
+const listaItens = document.getElementById('listaItens') as HTMLUListElement;
+const inputItem = document.getElementById('item') as HTMLInputElement;
+
+const carregarItens = (): Item[] => {
+    const itens = localStorage.getItem('itens');
+    return itens ? JSON.parse(itens) : [];
+};
